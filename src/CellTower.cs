601634a70c0b977927c95aa1c8004fc39e8cb50e
Arno <arnojooste3008@gmail.com>
2025-04-25 16:12:35 +0200

    Project setup completed with basic class implementation for the CellTower class

---
+++ src/CellTower.cs
@@ -0,0 +1,46 @@
+namespace CellTower
+{
+    public class CellTower
+    {
+        private string id { get; }
+        private int easting { get; }
+        private int northing { get; }
+        private double longitude { get; }
+        private double latitude { get; }
+
+        public CellTower(string id, int easting, int northing, double longitude, double latitude)
+        {
+            this.id = id;
+            this.easting = easting;
+            this.northing = northing;
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+
+        // GETTERS
+        public string getID()
+        {
+            return this.id;
+        }
+
+        public int getEasting()
+        {
+            return this.easting;
+        }
+
+        public int getNorthing()
+        {
+            return this.northing;
+        }
+
+        public double getLongitude()
+        {
+            return this.longitude;
+        }
+
+        public double getLatitude()
+        {
+            return this.latitude;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using CellTower;
+
+namespace CLICellFreqAllocator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project setup complete");
+        }
+    }
+}
